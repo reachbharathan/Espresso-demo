697481d5883f51afc23d285d9755b0e800bcd421
Kanthivel G <gkanthi@thoughtworks.com>
2019-05-29 19:32:06 +0530

    Implemented radio picker View

--- app/src/main/java/com/example/kanthivelg/espresso/EspressoRadioSpinnerActivity.kt
+++ app/src/main/java/com/example/kanthivelg/espresso/EspressoRadioSpinnerActivity.kt
@@ -1,13 +1,28 @@
 package com.example.kanthivelg.espresso
 
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.widget.RadioButton
+import kotlinx.android.synthetic.main.radio_spinner_activity.*
 
 class EspressoRadioSpinnerActivity: AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.radio_spinner_activity)
+        registerButton.setOnClickListener {
+            val name =  nameEditText.text.toString()
+            val selectedRadioButtonId = radioGroup.checkedRadioButtonId
+            val gender = findViewById<RadioButton>(selectedRadioButtonId).text
+            val dob = "${dobDatePicker.dayOfMonth}:${dobDatePicker.month}:${dobDatePicker.year}"
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("Registered")
+            builder.setMessage("You are a $gender with name $name and DOB $dob")
+            builder.setNeutralButton("OK") { _, _ ->}
+            builder.create().show()
+        }
     }
 
 }
