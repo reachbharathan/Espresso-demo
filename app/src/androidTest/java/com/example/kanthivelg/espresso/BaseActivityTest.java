da21e510b173f4a5f8515cb8e2bc3767722afd3a
Kumaran, Bharathan <bharathan.kumaran@kroger.com>
2019-05-29 19:37:22 +0530

    recyler view test temp

---
+++ app/src/androidTest/java/com/example/kanthivelg/espresso/BaseActivityTest.java
@@ -0,0 +1,18 @@
+package com.example.kanthivelg.espresso;
+
+import android.Manifest;
+
+import androidx.test.rule.GrantPermissionRule;
+
+import org.junit.Rule;
+import org.junit.rules.TestName;
+
+class BaseActivityTest {
+    @Rule
+    public GrantPermissionRule permissionRule =
+            GrantPermissionRule.grant(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+    @Rule public TestName name = new TestName();
+
+    public CustomFailureHandler customFailureHandler;
+}
--- app/src/androidTest/java/com/example/kanthivelg/espresso/RecyclerViewTest.java
+++ app/src/androidTest/java/com/example/kanthivelg/espresso/RecyclerViewTest.java
@@ -1,22 +1,44 @@
 package com.example.kanthivelg.espresso;
 
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.v7.widget.RecyclerView;
+
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import androidx.test.espresso.Espresso;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+import static com.example.kanthivelg.espresso.RecyclerViewMatcher.withRecyclerView;
+
 @RunWith(AndroidJUnit4.class)
-public class RecyclerViewTest {
+public class RecyclerViewTest extends BaseActivityTest{
 
     @Rule
     public ActivityTestRule<EspressoRecyclerViewActivity> activityTestRule =
             new ActivityTestRule<>(EspressoRecyclerViewActivity.class);
 
+    @Before
+    public void setUp() {
+        customFailureHandler = new CustomFailureHandler(
+                getInstrumentation().getTargetContext(),
+                activityTestRule.getActivity().getLocalClassName(), activityTestRule.getActivity());
+        Espresso.setFailureHandler(customFailureHandler);
+    }
+
+    // Convenience helper
+
 
     @Test
-    public void testEventList() {
+    public void testFirstItem() {
+        onView(withRecyclerView()).perform(RecyclerViewActions.actionOnItem())
+                ;
 
     }
 }
